3f3579645f8470f0bed2fbbbce2a6c010869d4a9
Fab-one <info@fab-one.nl>
2018-06-09 21:55:57 +0200

    shoppingcart 3 passed, pathfind 3 passed

--- src/pathFind.js
+++ src/pathFind.js
@@ -1,7 +1,28 @@
 function pathFind(path, object) {
 
+
+
+
+      const obj = {}
+          path
+            .filter(p => p.object === object)
+            .forEach(p =>
+                obj[p.id] = pathFind(path, p.id))
+        return obj
+        return path
 }
 
 
 
 module.exports = { pathFind }
+
+// pathFind(["foo"], { foo: "bar" }) // === "bar"
+// pathFind(["name"], { name: "Dave" }) // === "Dave"
+// pathFind(["author", "name"], { author: { name: "Stephen" } }) // === "Stephen"
+// pathFind(["book", "author", "name"], {
+//   book: {
+//     author: {
+//       name: "Yuval"
+//     }
+//   }
+// }) // === "Yuval"
