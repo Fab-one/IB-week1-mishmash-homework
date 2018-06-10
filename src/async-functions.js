8566f086d195ba7ae05002b12f363b2d0fad79c2
Fab-one <info@fab-one.nl>
2018-06-10 14:07:24 +0200

    shoppingcart 3 passed, pathfind 4 passed, async 2 passed

--- src/async-functions.js
+++ src/async-functions.js
@@ -1,13 +1,14 @@
 function giveItBackLater(value, callback) {
-
+          callback(value)
 }
+setTimeout(giveItBackLater, 2000)
 
 function promiseToGiveItBackLater(value) {
 
 }
 
 function addSomePromises(somePromise) {
-  
+
 }
 
 module.exports = {  giveItBackLater, addSomePromises, promiseToGiveItBackLater}
